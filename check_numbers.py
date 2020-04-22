d2149c5e89683e38d5d3b211fbffff1c13b7a476
kamilko250 <freshmultikk21@gmail.com>
2020-04-22 22:53:09 +0200

    Update check_numbers.py

--- check_numbers.py
+++ check_numbers.py
@@ -6,14 +6,18 @@ if(len(sys.argv) > 1):
           filename = sys.argv[1]
 else:
      filename = 0
-     print("No file given.")
+     print("No file to check. Use 'python check_numbers.py filetocheckname.txt'")
 if(filename != 0):     
      file = open(filename, "r")
      line = file.readline()
+     result = "["
      while( line != ''):
           splitedline = line.split()  
           splitedline[1] = int(splitedline[1],16)
+          line = file.readline()
           if(isprime(splitedline[1])):
-               print (splitedline[0])
-          line = file.readline()        
+               result = result + splitedline[0] 
+               if(line != ''):
+                    result = result + ", "
      file.close()
+     print (result + "]")
