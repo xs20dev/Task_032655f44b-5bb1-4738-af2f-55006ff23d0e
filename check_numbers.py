33e4fd8894746f7d1fc803078cae1ef30089f735
kamilko250 <freshmultikk21@gmail.com>
2020-04-22 22:42:26 +0200

    Update check_numbers.py
    Working program

--- check_numbers.py
+++ check_numbers.py
@@ -1,1 +1,19 @@
 #Napisz program
+import sys
+from sympy import isprime
+if(len(sys.argv) > 1):
+     if(sys.argv[1] != 0):
+          filename = sys.argv[1]
+else:
+     filename = 0
+     print("No file given.")
+if(filename != 0):     
+     file = open(filename, "r")
+     line = file.readline()
+     while( line != ''):
+          splitedline = line.split()  
+          splitedline[1] = int(splitedline[1],16)
+          if(isprime(splitedline[1])):
+               print (splitedline[0])
+          line = file.readline()        
+     file.close()
